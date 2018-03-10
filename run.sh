a8c8b51aa199e15142052f80842727e19c725535
Chen Weiguang <chen.weiguang@gmail.com>
2018-03-11 00:25:44 +0800

    Add init.vim config

--- run.sh
+++ run.sh
@@ -3,6 +3,17 @@
 # install neovim
 sudo apt-get install -y neovim
 
+# install neovim Plug package manager
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# set up all plugins
+mkdir -p ~/.config/nvim
+cp ./init.vim ~/.config/nvim/
+
+# install all plug
+nvim -c :PlugInstall -c :q -c :q -c :q
+
 # install powerline-fonts
 git clone https://github.com/powerline/fonts.git powerline-fonts
 sudo ./powerline-fonts/install.sh
